9da8113dc8a99208ce96cb385b03d8ae06fa2989
Rusilah Gojali <rgojali@gmail.com>
2018-10-30 01:21:38 -0700

    added index.js

---
+++ hw2/index.js
@@ -0,0 +1,5 @@
+function greet (firstName, lastName, superheroName, greeting){
+    return `${greeting} ${firstName} ${lastName} ${superheroName}`
+  }
+  greet('Rusilah', 'Gojali', 'aka Super Rutastic', 'Hi')
+
