c7cba7c4b84aea3dd132c33657d0e54f11d299f0
Rusilah Gojali <rgojali@gmail.com>
2018-12-12 20:11:15 -0800

    added a function to index.js

--- hw-wk9/index.js
+++ hw-wk9/index.js
@@ -5,5 +5,10 @@
 console.log(Math.floor(Math.random() * 10))
 
 // 2. Find the remainder of 5/2
+//Write a function to get the remainder of 5/2
 
-console.log(5 % 2)
+function sunshine(a, b) {
+    return a % b
+}
+
+console.log(sunshine(5, 2))
